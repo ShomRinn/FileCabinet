ab74a60d83e2da6983a0fa8b955a708e269f217e
Georgi Bachilo <bachyla.heorhi@student.ehu.lt>
2025-01-13 16:41:11 +0200

    Merge step6-export into main: Add export functionality (CSV and XML).

---
+++ FileCabinetApp/Writers/FileCabinetRecordCsvWriter.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using FileCabinetApp.Models;
+
+namespace FileCabinetApp.Writers
+{
+    public class FileCabinetRecordCsvWriter
+    {
+        private readonly TextWriter writer;
+
+        public FileCabinetRecordCsvWriter(TextWriter writer)
+        {
+            this.writer = writer;
+        }
+
+        public void Write(FileCabinetRecord record)
+        {
+            ArgumentNullException.ThrowIfNull(record, nameof(record));
+            this.writer.WriteLine($"{record.Id},{record.FirstName},{record.LastName},{record.DateOfBirth:MM/dd/yyyy},{record.Height},{record.Salary},{record.Gender}");
+        }
+
+        public void WriteHeader()
+        {
+            this.writer.WriteLine("Id,FirstName,LastName,DateOfBirth,Height,Salary,Gender");
+        }
+    }
+}
---
+++ FileCabinetApp/Writers/FileCabinetRecordXmlWriter.cs
@@ -0,0 +1,47 @@
+﻿using System.Globalization;
+using System.Xml;
+using FileCabinetApp.Models;
+
+namespace FileCabinetApp.Writers
+{
+    public class FileCabinetRecordXmlWriter
+    {
+        private readonly XmlWriter writer;
+
+        public FileCabinetRecordXmlWriter(XmlWriter writer)
+        {
+            this.writer = writer ?? throw new ArgumentNullException(nameof(writer));
+        }
+
+        public void Write(FileCabinetRecord record)
+        {
+            ArgumentNullException.ThrowIfNull(record);
+
+            this.writer.WriteStartElement("record");
+            this.writer.WriteAttributeString("id", record.Id.ToString(CultureInfo.InvariantCulture));
+
+            this.writer.WriteStartElement("name");
+            this.writer.WriteAttributeString("first", record.FirstName);
+            this.writer.WriteAttributeString("last", record.LastName);
+            this.writer.WriteEndElement();
+
+            this.writer.WriteStartElement("dateOfBirth");
+            this.writer.WriteString(record.DateOfBirth.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture));
+            this.writer.WriteEndElement();
+
+            this.writer.WriteStartElement("height");
+            this.writer.WriteString(record.Height.ToString(CultureInfo.InvariantCulture));
+            this.writer.WriteEndElement();
+
+            this.writer.WriteStartElement("salary");
+            this.writer.WriteString(record.Salary.ToString(CultureInfo.InvariantCulture));
+            this.writer.WriteEndElement();
+
+            this.writer.WriteStartElement("gender");
+            this.writer.WriteString(record.Gender.ToString(CultureInfo.InvariantCulture));
+            this.writer.WriteEndElement();
+
+            this.writer.WriteEndElement();
+        }
+    }
+}
